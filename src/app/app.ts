5620e724153109458b0078fdc4ed0b1ab05848ae
Daniel Sanchez Gonzalez <sanchezgonzalezdaniel3@gmail.com>
2025-06-18 20:54:26 -0600

    P07 - Event Binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Padre } from './padre/padre';
 import { ComponenteEnLinea } from "./componente-en-linea/componente-en-linea";
 import { Hijo } from "./hijo/hijo";
+import { MostrarMensaje } from "./mostrar-mensaje/mostrar-mensaje";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Hijo],
+  imports: [MostrarMensaje],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
