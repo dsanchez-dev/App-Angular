ef7d3d8864d21b649183112cdeee7e3c36bc646c
Daniel Sanchez Gonzalez <sanchezgonzalezdaniel3@gmail.com>
2025-06-19 23:39:57 -0600

    P08 - Property Binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,13 @@
 import { Component } from '@angular/core';
-import { Padre } from './padre/padre';
-import { ComponenteEnLinea } from "./componente-en-linea/componente-en-linea";
-import { Hijo } from "./hijo/hijo";
-import { MostrarMensaje } from "./mostrar-mensaje/mostrar-mensaje";
+import { Replicador } from "./replicador/replicador";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MostrarMensaje],
+  imports: [Replicador],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected titulo = 'Hola Mundo desde Angular!';
+  protected titulo = 'Property binding en Angular';
 }
