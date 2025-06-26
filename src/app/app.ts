80a99bfcde00365eb23c14a47adc8f193fbda252
Daniel Sanchez Gonzalez <sanchezgonzalezdaniel3@gmail.com>
2025-06-26 10:56:11 -0600

    P11 - Local Reference

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { ComponenteIf } from './componente-if/componente-if';
+import { AgregarTarea } from "./agregar-tarea/agregar-tarea";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ComponenteIf],
+  imports: [AgregarTarea],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  titulo = 'Directiva @if en Angular';
+  titulo = 'Local Reference en Angular';
 }
